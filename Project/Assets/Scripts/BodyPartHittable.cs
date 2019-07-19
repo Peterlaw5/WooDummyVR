7965532403b94613c9968ec3387281e7559db825
Peterlaw <pietro.peterlaw@gmail.com>
2019-07-19 12:09:46 +0200

    WooDummyVR

---
+++ Project/Assets/Scripts/BodyPartHittable.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BodyPartHittable : MonoBehaviour
+{
+    public AudioSource hit;
+    public AudioSource fail;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if ((GetComponent<Renderer>().material.color == Color.blue && other.gameObject.tag == "Lglove") || (GetComponent<Renderer>().material.color == Color.green && other.gameObject.tag == "Rglove"))
+        {
+            hit.Play();
+            ++ScoreManager.score;
+            GetComponent<Renderer>().material.color = Color.white;
+        }
+        else
+        {
+            fail.Play();
+        }
+    }
+
+
+}
---
+++ Project/Assets/Scripts/BodyPartsBehaviour.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BodyPartsBehaviour : MonoBehaviour
+{
+    public GameObject[] bodyParts;
+    private Color[] colors = {Color.green , Color.blue};
+    public Color defaultColor = Color.white;
+    public bool isColored = false;
+    public int score = 0;
+    public Text scoreTxt;
+
+    void CambioColore()
+    {
+        for(int i = 1; i < bodyParts.Length; i++)
+        {
+            bodyParts[(int)Random.Range(0, 4)].GetComponent<Renderer>().material.color = colors[(int)Random.Range(0, 2)];
+        }
+
+        isColored = true;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (bodyParts[0].GetComponent<Renderer>().material.color == Color.white && bodyParts[1].GetComponent<Renderer>().material.color == Color.white && bodyParts[2].GetComponent<Renderer>().material.color == Color.white && bodyParts[3].GetComponent<Renderer>().material.color == Color.white)
+        {
+            isColored = false;
+        }
+
+        if (!isColored)
+        {
+            CambioColore();
+        }
+
+        print(ScoreManager.score);
+        scoreTxt.text = "Score: " + ScoreManager.score.ToString();
+    }
+}
